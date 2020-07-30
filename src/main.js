b865177e9b65803d9a5011504c2aadfc8f1fea44
Daniel <1375460391@qq.com>
2020-07-30 19:27:39 +0800

    'sixth'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from "./store";
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,11 @@ Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
 
+//解决移动端的300ms延时
+FastClick.attach(document.body)
+
+//使用vue-lazyload懒加载
+Vue.use(VueLazyLoad)
 new Vue({
   render: h => h(App),
   router,
